dfc46b814f8077fe14fe337df2da3526f30cbefb
PhantomX-13 <tanzimur.rahman2445@gmail.com>
2026-03-04 23:45:43 +0600

    4 March, 2026 update

--- Phase 1 - Part One/Practise 3.1/I_Count_Vowels.cpp
+++ Phase 1 - Part One/Practise 3.1/I_Count_Vowels.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <set>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <limits>
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
 using namespace std;
+
+int countVowels(string s)
+{
+    if(s.empty())
+        return 0;
+    
+    char ch = tolower(s.back());
+
+    s.pop_back();
+
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        return 1 + countVowels(s);
+    else
+        return countVowels(s);
+}
 
 int main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
+
+    string s;
+    getline(cin, s);
+
+    cout << countVowels(s) << '\n';
 
     
     return 0;
