6c5e8373ce4f8b7a2a54b40e0dc6c01df951856d
MD. Tanzimur Rahman <tanzimur.rahman2445@gmail.com>
2026-08-28 17:04:57 +0600

    28 Aug, 2026

--- Phase 1 - Part One/Practise 3.1/V_Creating_Expression_1.cpp
+++ Phase 1 - Part One/Practise 3.1/V_Creating_Expression_1.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
 using namespace std;
 
+int n;
+int expected_sum;
+int a[25];
+bool found = false;
+
+void solve(int i, int total_sum)
+{
+    if(found)
+        return;
+
+    if(i>n)
+    {
+        if(total_sum == expected_sum)
+        {
+            found = true;
+        }
+        return;
+    }
+
+    solve(i+1, total_sum + a[i]);
+
+    solve(i + 1, total_sum - a[i]);
+}
+
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    
+    if(!(cin >> n >> expected_sum))
+        return 0;
+
+    for(int i=1; i<=n; i++)
+    {
+        cin >> a[i];
+    }
+
+    solve(2, a[1]);
+
+    if (found)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+
     return 0;
 }
