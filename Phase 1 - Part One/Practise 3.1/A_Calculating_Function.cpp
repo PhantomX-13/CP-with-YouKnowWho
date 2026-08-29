43800dd6a541f67bec56fb4cb0c08f5136466984
MD. Tanzimur Rahman <tanzimur.rahman2445@gmail.com>
2026-08-30 00:47:36 +0600

    30 Aug, 2026

---
+++ Phase 1 - Part One/Practise 3.1/A_Calculating_Function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    if(n%2==0)
+    {
+        cout << n/2 << '\n';
+    }
+    else{
+        cout << -(n+1)/2 << '\n';
+    }
+    return 0;
+}
