79e0a0f753f4df4e6157b1e80edccf7afb5ab8f5
MD. Tanzimur Rahman <tanzimur.rahman2445@gmail.com>
2026-08-29 08:49:05 +0600

    29 August, 2026

---
+++ Phase 1 - Part One/Practise 3.1/A_Even_Odds.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    ll n, k;
+    cin >> n >> k;
+
+    ll odd_count = (n+1)/2;
+    ll rslt = 0;
+
+    if(k <= odd_count)
+    {
+        rslt = 2*k - 1;
+    }
+    if(k > odd_count)
+    {
+        rslt = 2 * (k - odd_count);
+    }
+
+    cout << rslt << '\n';
+    return 0;
+}
